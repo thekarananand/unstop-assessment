cc651917c5540c8b7a1db98a5679c9380eefada1
Karan Anand <thekarananand@gmail.com>
2024-09-08 16:34:12 +0530

    feat: basic logic

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from utils import BookInSingleRow, BookFullCluster, BookLargerCluster
+
+# state = [ 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 3 ]
+state = [ 0, 2, 1, 0, 1, 2, 1, 0, 0, 0, 2, 0 ]
 
 app = FastAPI()
 app.add_middleware(
@@ -14,16 +18,106 @@ app.add_middleware(
 class DataScheme(BaseModel):
     SeatsNeeded: int
 
+@app.get("/reset")
+async def reset():
+    global state
+    state = [ 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 3 ]
+    return { "State"  : state }
+
 @app.post("/")
-async def read_root(RequestBody: DataScheme):
+async def index(RequestBody: DataScheme):
+
+    global state
     n = RequestBody.SeatsNeeded
 
     if not ( 1 <= n and n <= 7 ):
-        return { "Error": f'${n} is out of bound' }
+        return { "Error": f'${n} is a valid input' }
     
-    return { "Seat" : [
-        0,
-        55,
-        69,
-        79
-    ] }
+    if ( sum(state) < n ):
+        return {
+            "Error"  : "We don't have Reqiured Capacity",
+            "Booked" : [],
+            "State"  : state
+        }
+    
+    # Check for Rows with Requested Capacity.
+    suitable_row = -1;
+    suitable_row_state = 8;
+    for row in range(len(state)):
+        if ( (state[row] >= n) and (suitable_row_state > state[row]) ):
+            suitable_row = row
+            suitable_row_state = state[row]
+            if (state[row] == n):
+                break
+
+    # Assigning Seats, if a Row with Requested Capacity is found.
+    if ( suitable_row != -1 ):
+        
+        [ state, booked ] = BookInSingleRow(state=state, row=suitable_row, seats=n)
+
+        return { 
+            "Booked" : booked,
+            "State"  : state
+        }
+    
+    # Assigning Seats, in multiple Rows.
+    else:
+        clusters = {}
+        prefix_sum = 0
+        begin = -1
+        
+        for row in range(len(state)):
+            prefix_sum += state[row]
+            if begin == -1 and state[row] != 0:
+                begin = row
+            if (state[row] == 0):
+                clusters[begin]  = prefix_sum
+
+
+        clusters = dict(sorted(clusters.items(), key=lambda item: item[1], reverse=True))
+
+        suitable_single_cluster = -1
+        for cluster in clusters:
+            if (clusters[cluster] == n):
+
+                [ state, booked ] = BookFullCluster(state=state, cluster=cluster)
+
+                return { 
+                    "Booked" : booked,
+                    "State"  : state
+                }
+            
+            elif (clusters[cluster] > n):
+                if (suitable_single_cluster == -1):
+                    suitable_single_cluster = cluster
+                elif ( clusters[suitable_single_cluster] > clusters[cluster] ):
+                    suitable_single_cluster = cluster
+            
+        if (suitable_single_cluster != -1):
+
+            [ state, booked ] = BookLargerCluster(state=state, cluster=cluster, seats=n)
+
+            return { 
+                "Booked" : booked,
+                "State"  : state
+            }
+
+        # Assign Seats in More then one clusters
+        else:
+            m = n
+            booked = []
+            for cluster in clusters:
+                if ( clusters[cluster] <= m):
+                    m -= clusters[cluster]
+                    [ state, subset_booked ] = BookFullCluster(state=state, cluster=cluster)
+                    
+                    booked.extend(subset_booked)
+                else:
+                    pass
+                    [ state, subset_booked ] = BookLargerCluster(state=state, cluster=cluster, seats=m)
+                    
+                    booked.extend(subset_booked)
+            return { 
+                "Booked" : booked,
+                "State"  : state
+            }
---
+++ Backend/utils.py
@@ -0,0 +1,47 @@
+def BookInSingleRow(state, row, seats):
+
+    begin = -1
+
+    if ( row == 11 ):
+        begin = 77 + ( 3 - state[row] + 1)
+    else:
+        begin = ( row * 7 ) + ( 7 - state[row] + 1)
+
+    state[row] -= seats
+
+    return [ state, list(range( begin, begin + seats )) ]
+
+def BookFullCluster(state, cluster):
+    
+    row = cluster
+    booked = []
+
+    while (state[row] != 0):
+        [ state, subset_booked ] = BookInSingleRow(state, row, state[row])
+        booked.extend(subset_booked)
+        row += 1
+
+    return [ state, booked ]
+
+
+def BookLargerCluster(state, cluster, seats):
+    print("/n/n++++++++++++++++++++++++++++++")
+    print(seats)
+    print("++++++++++++++++++++++++++++++/n/n")
+    
+    row = cluster
+    m = seats
+    booked = []
+
+    while ( m != 0):
+        subset_booked = []
+        if ( m > state[row] ):
+            m -= state[row]
+            [ state, subset_booked ] = BookInSingleRow(state, row, state[row])
+        else:
+            [ state, subset_booked ] = BookInSingleRow(state, row, m)
+            m = 0
+        booked.extend(subset_booked)
+        row += 1
+
+    return [ state, booked ]
