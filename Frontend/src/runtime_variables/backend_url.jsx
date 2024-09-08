15ba0c5c6573dfe4160ddb254ecd28a0b43c977d
Karan Anand <thekarananand@gmail.com>
2024-09-08 23:16:22 +0530

    fix: backend env

--- Frontend/src/runtime_variables/backend_url.jsx
+++ Frontend/src/runtime_variables/backend_url.jsx
@@ -1,3 +1,2 @@
-// const backend_url = process.env.BACKEND_URL;
-const backend_url = "http://thekarananand-unstop-backend.hqfubdawa0b7bbbb.centralindia.azurecontainer.io/";
+const backend_url = process.env.BACKEND_URL;
 export default backend_url;
