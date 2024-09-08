c5a23d8bca79d555800bf590799ee1d48327efcd
Karan Anand <thekarananand@gmail.com>
2024-09-09 04:14:30 +0530

    fix: minor fix

--- Frontend/src/runtime_variables/backend_url.jsx
+++ Frontend/src/runtime_variables/backend_url.jsx
@@ -1,3 +1,2 @@
-// const backend_url = process.env.BACKEND_URL;
-const backend_url = 'https://unstop-api.azurewebsites.net/';
+const backend_url = 'https://unstop-api.azurewebsites.net/'
 export default backend_url;
